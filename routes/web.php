9e6d234948a229966ab5c947150d0803c0937335
Dnnug <daniswaradarmawantriputra@gmail.com>
2026-04-04 14:20:31 +0700

    Update Route untuk Data Blocks serta memberikan comment ke seluruh route

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,26 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\LandingController;
+use App\Http\Controllers\BlockController;
 use Illuminate\Support\Facades\Route;
+/* INi Route Landing Page */
 
 Route::get('/', [LandingController::class, 'index']);
-
+/* Ini ROute Middleware untuk login dan menuju dashboard */
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+/** Ini Route Middleware + CRUD untuk Profile */
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+/** INI Route untuk CRUD Blocks */
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('blocks', BlockController::class);
 });
 
 require __DIR__ . '/auth.php';

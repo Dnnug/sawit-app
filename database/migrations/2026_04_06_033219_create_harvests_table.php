94b809300c9d988b2663d543bce63b18b3286973
Dnnug <daniswaradarmawantriputra@gmail.com>
2026-04-06 13:14:06 +0700

    Setup Migrasi untuk Harvest

---
+++ database/migrations/2026_04_06_033219_create_harvests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('harvests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('block_id')->constrained()->onDelete('cascade');
+            $table->date('harvest_date');
+            $table->double('weight_count');
+            $table->integer('worker_count');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('harvests');
+    }
+};
